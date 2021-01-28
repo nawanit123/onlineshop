dcd33028ff947ece0bf27097c7b80888aea70acb
Nawanit <sharma.nawanit123@gmail.com>
2021-01-28 15:10:16 +0530

    Added header,footer and react-bootstrap to the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+
 const App = () => {
   return (
     <div className="App">
       <>
-        <h1>Welcome to MyShop</h1>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <h1>Welcome to MyShop</h1>
+          </Container>
+        </main>
+        <Footer />
       </>
     </div>
   );

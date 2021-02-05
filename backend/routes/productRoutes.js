0709c57645c10f1e7e89856e98b9a769608571e8
Nawanit <sharma.nawanit123@gmail.com>
2021-02-05 15:34:31 +0530

    Added productRoutes as an independednt structure with async handler

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+const router = express.Router();
+import Product from '../models/productModel.js';
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @acees   Public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json(products);
+  })
+);
+
+// @desc    Fetches single product
+// @route   GET /api/products/:id
+// @acees   Public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    console.log(product);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Product not Found' });
+    }
+  })
+);
+
+export default router;

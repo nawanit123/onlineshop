1c822dcab9386c3f14494e36681a5640a5f5237d
Nawanit <sharma.nawanit123@gmail.com>
2021-02-15 15:50:25 +0530

    Created user action,reducers and made persistent with localStorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,19 +7,25 @@ import {
   productListReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const carItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : [];
 
 const initialState = {
   cart: { cartItems: carItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
